1728665cd30b6a39ce88b3c5eb46232defe82309
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-04-15 19:39:03 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+const menuBtn = document.querySelector('.menu-btn')
+const sideBar = document.querySelector('aside')
+const mainContent = document.querySelector('main')
+
+menuBtn.addEventListener('click', toogleSideBar)
+
+function toogleSideBar() {
+  sideBar.classList.toggle('close')
+  mainContent.classList.toggle('close')
+}
